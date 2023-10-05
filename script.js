02b06b1f7a0c347bd988fc93b8ef8c2f42da17c4
JehadMari <96633026+JehadMari@users.noreply.github.com>
2023-10-05 14:53:40 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+        // Set the event date (replace with your event date)
+        const eventDate = new Date("2023-11-31T23:59:59").getTime();
+
+        // Update the countdown every second
+        const countdown = setInterval(function () {
+            const currentDate = new Date().getTime();
+            const timeLeft = eventDate - currentDate;
+
+            const days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+            const hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            const minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+            const seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+
+            document.getElementById("days").innerText = days.toString().padStart(2, '0');
+            document.getElementById("hours").innerText = hours.toString().padStart(2, '0');
+            document.getElementById("minutes").innerText = minutes.toString().padStart(2, '0');
+            document.getElementById("seconds").innerText = seconds.toString().padStart(2, '0');
+
+            if (timeLeft < 0) {
+                clearInterval(countdown);
+                document.getElementById("days").innerText = "00";
+                document.getElementById("hours").innerText = "00";
+                document.getElementById("minutes").innerText = "00";
+                document.getElementById("seconds").innerText = "00";
+            }
+        }, 1000);
